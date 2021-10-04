4312f09a62f988441600cfcad06e0bf84da35f64
Julian V <julivehs@gmail.com>
2021-10-04 16:12:57 +0200

    Initial

---
+++ src/main/java/H04/CorrectnessCheck.java
@@ -0,0 +1,45 @@
+package H04;
+import fopbot.*;
+
+public class CorrectnessCheck {
+
+  final int BASIC_ARRAY_SIZE;
+
+
+  public CorrectnessCheck(final int BASIC_ARRAY_SIZE) {
+    this.BASIC_ARRAY_SIZE = BASIC_ARRAY_SIZE;
+  }
+
+  public void run() {
+    init();
+    System.out.println("1. Überprüfung: " + check1());
+    swap();
+    System.out.println("2. Überprüfung: " + check2());
+    System.out.println("3. Überprüfung: " + check3());
+  }
+
+
+  private void init() {
+    //TODO
+  }
+
+  private boolean check1() {
+    //TODO
+    throw new UnsupportedOperationException("Noch nicht implementiert");
+  }
+
+  private void swap() {
+    //TODO
+  }
+
+  private boolean check2() {
+    //TODO
+    throw new UnsupportedOperationException("Noch nicht implementiert");
+  }
+
+  private boolean check3() {
+    //TODO
+    throw new UnsupportedOperationException("Noch nicht implementiert");
+  }
+
+}
--- src/main/java/H04/Main.java
+++ src/main/java/H04/Main.java
@@ -1,7 +1,30 @@
 package H04;
 
+import fopbot.*;
+
 public class Main {
+
+  public static int WORLD_SIZE_X = 10;
+  public static int WORLD_SIZE_Y = 10;
+  public static int BASIC_ARRAY_SIZE = 5;
+
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    World.setSize(WORLD_SIZE_X, WORLD_SIZE_Y);
+    World.setDelay(200);
+    World.setVisible(true);
+
+    //Playground
+    robotTests();
+
+    //H3
+    CorrectnessCheck correctnesCheck = new CorrectnessCheck(BASIC_ARRAY_SIZE);
+//		correctnesCheck.run();
+
   }
+
+  public static void robotTests() {
+    //TODO
+  }
+
+
 }
