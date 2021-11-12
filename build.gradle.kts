dbbaeb67166a6077c5b964d93111e4abe9161ee1
Alexander Staeding <alexanderstaeding@gmail.com>
2021-11-12 21:02:46 +0100

    Update JUnit to v5.8.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,7 +28,7 @@ dependencies {
   // FOPBot available in all source sets
   implementation("org.sourcegrade:fopbot:0.3.0")
   // JUnit only available in "test" source set (./src/test)
-  testImplementation("org.junit.jupiter:junit-jupiter:5.7.2")
+  testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
 }
 
 java {
