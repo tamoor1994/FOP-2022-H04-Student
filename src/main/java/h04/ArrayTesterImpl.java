0ba94279791503f75ea30fe00d00ea02b5185b9d
tamoor1994 <73400071+tamoor1994@users.noreply.github.com>
2023-06-28 22:47:33 +0200

    Tamoor Hussain

--- src/main/java/h04/ArrayTesterImpl.java
+++ src/main/java/h04/ArrayTesterImpl.java
@@ -13,11 +13,32 @@ public class ArrayTesterImpl implements ArrayTester {
 
     @Override
     public boolean test3(WithNaturalCoordinates[] array3) {
-        return false;
+        for (int i = 0; i < array3.length; i++) {
+            if (array3[2 * i].getX() != 2 * i || array3[2 * i].getY() != i) {
+                return false;
+            }
+            if (array3[2 * i].getX() != getValueOfAxis(Axis.values()[i % 3], i) || array3[2 * i].getY() != getValueOfAxis(Axis.values()[(i % 3) % 3], i)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     @Override
     public boolean test4(WithNaturalCoordinates[] array4) {
         return false;
+    }
+
+    private int getValueOfAxis(Axis axis, int index) {
+        switch (axis) {
+            case X:
+                return 3 * index;
+            case Y:
+                return 4 * index;
+            case Z:
+                return 5 * index;
+            default:
+                throw new IllegalArgumentException("Fehler!!");
+        }
     }
 }
